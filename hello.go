a7cd574c03f892c9842718d969bbd360976762b0
森田晃史 <akifumi.morita@gmail.com>
2020-07-21 14:57:10 +0900

    [Update]セクション７fun-out,in

--- hello.go
+++ hello.go
@@ -1,18 +1,37 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func producer(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
+func multi2(first chan int, second chan int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
+	}
+}
+
+func multi4(second chan int, third chan int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
+}
 
 func main() {
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-	close(ch)
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
 
-	for c := range ch {
-		fmt.Println(c)
+	go producer(first)
+	go multi2(first, second)
+	go multi4(second, third)
+	for result := range third {
+		fmt.Println(result)
 	}
 }
