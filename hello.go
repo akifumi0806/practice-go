9b55ed431b45cce0aedaaaf298ed0fc887e68a0c
森田晃史 <akifumi.morita@gmail.com>
2020-07-22 14:46:37 +0900

    [Update]セクション７演習問題

--- hello.go
+++ hello.go
@@ -2,36 +2,20 @@ package main
 
 import "fmt"
 
-func producer(first chan int) {
-	defer close(first)
-	for i := 0; i < 10; i++ {
-		first <- i
+func goroutine(s []string, c chan string) {
+	sum := ""
+	for _, v := range s {
+		sum += v
+		c <- sum
 	}
-}
-
-func multi2(first chan int, second chan int) {
-	defer close(second)
-	for i := range first {
-		second <- i * 2
-	}
-}
-
-func multi4(second chan int, third chan int) {
-	defer close(third)
-	for i := range second {
-		third <- i * 4
-	}
+	close(c)
 }
 
 func main() {
-	first := make(chan int)
-	second := make(chan int)
-	third := make(chan int)
-
-	go producer(first)
-	go multi2(first, second)
-	go multi4(second, third)
-	for result := range third {
-		fmt.Println(result)
+	words := []string{"test1!", "test2!", "test3!", "test4!"}
+	c := make(chan string)
+	go goroutine(words, c)
+	for w := range c {
+		fmt.Println(w)
 	}
 }
